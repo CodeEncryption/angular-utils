fa0e67cbfa9e5865fa0015bd757ae9a57d74f028
CodeEncryption <xmutpsj@163.com>
2019-04-02 10:38:34 +0800

    util-score's init option set by @Input() option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Option } from "./utils/score/score.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-utils';
+  option:Option={
+    num: 7,
+    score: 4,
+    position: 'end',
+    disable: false
+  }
   getScore(n){
     console.log(n);
   }
--- src/app/utils/score/score.component.ts
+++ src/app/utils/score/score.component.ts
@@ -1,49 +1,71 @@
-import { 
-  Component, 
-  OnInit, 
-  Input, 
-  Output, 
-  EventEmitter, 
-  ViewChild, 
-  ElementRef, 
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
   Renderer2
-} from '@angular/core';
+} from "@angular/core";
+
+export interface Option {
+  num: number;
+  score?: number;
+  position?: string;
+  disable?: boolean;
+}
 
 @Component({
-  selector: 'util-score',
-  templateUrl: './score.component.html',
-  styleUrls: ['./score.component.css']
+  selector: "util-score",
+  templateUrl: "./score.component.html",
+  styleUrls: ["./score.component.css"]
 })
 export class ScoreComponent implements OnInit {
-  @Output() select=new EventEmitter<number>();
-  _num = 5;
-  items=[];
-  status=[];
-  @Input() 
-  set num(num:number){
-    this._num=(num<5?5:num);
-    this.items.length=this._num;
+  @Output() select = new EventEmitter<number>();
+  num = 5;
+  score = 3;
+  position = "center";
+  disable = false;
+  _option: Option = {
+    num: this.num,
+    score: this.score,
+    position: this.position,
+    disable: this.disable
   };
-  get num(): number { return this._num; }
-  @Input() score:number;
-  @Input() position:string;
-  @Input() disabled:string;
-  @ViewChild('pos') pos:ElementRef;
-  
-  constructor(private el:ElementRef,private rd:Renderer2) { }
-  
-  ngOnInit() {
-    this.rd.addClass(this.pos.nativeElement,this.position)
-    //初始化评分
-    this.star(this.score,true)
+  items = [];
+  status = [];
+  @Input()
+  set option(option: Option) {
+    this._option.num = option.num;
+    this._option.score = option.score;
+    this._option.position = option.position;
+    this._option.disable = option.disable;
+    this.items.length = this._option.num;
   }
-  star(n,flag=false){
-    if(!flag){if(this.disabled=='true'){return;}}
-    for(let i=0;i<n;i++){
-      this.status[i]=true;
+  get option(): Option {
+    return this._option;
+  }
+  @ViewChild("pos") pos: ElementRef;
+
+  constructor(private el: ElementRef, private rd: Renderer2) {}
+
+  ngOnInit() {
+    this.rd.addClass(this.pos.nativeElement, this.option.position);
+    //初始化评分
+    this.star(this.option.score, true);
+  }
+  star(n, flag = false) {
+    if (!flag) {
+      if (this.option.disable == true) {
+        return;
+      }
     }
-    for(let j=this.num-1;j>=n;j--){
-      this.status[j]=false;
+    for (let i = 0; i < n; i++) {
+      this.status[i] = true;
+    }
+    for (let j = this.option.num - 1; j >= n; j--) {
+      this.status[j] = false;
     }
     this.select.emit(n);
   }
